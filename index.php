685bbe2a9b20d9183ea6e5d10b30437ca77830cb
jakemerlin <jakemerlin123@gmail.com>
2023-11-11 18:56:17 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="list">
+            <div class="item">
+                <div class="img">
+                    <img src="images/house1.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 1
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house2.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 2
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house3.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 3
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house4.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 4
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/best.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 5
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house1.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 6
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/hylands.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 7
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="img">
+                    <img src="images/house.jpg">
+                </div>
+                <div class="content">
+                    <div class="title">
+                        House name 8
+                    </div>
+                    <div class="des">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, alias.
+                    </div>
+                    <div class="price">
+                        #203.5
+                    </div>
+                    <button class="add">Add to cart</button>
+                </div>
+            </div>
+        </div>
+
+
+        <ul class="listPage">
+            <li class="active">1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+
+
+    </div>
+
+    <script src="index.js"></script>
+</body>
+
+</html>
